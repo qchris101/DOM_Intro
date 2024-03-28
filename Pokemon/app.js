dfc0df318a68053669f7f67210b58baa2862745d
qchris101 <qchris101@yahoo.com>
2024-03-28 15:14:57 -0400

    Completed Lesson. Experimented more with Pokemon File.

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -2,18 +2,18 @@
 
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const h1 = document.querySelector('h1')
 
 
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
+
+for (let i = 1; i <= 1025; i++){
+    const pokemon = document.createElement('div')
+    const label = document.createElement('span')
+    pokemon.classList.add('pokemon')
+    label.innerText = `#${i}`
+    const newImg = document.createElement('img')
     newImg.src = `${baseURL}${i}.png`
-
-
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+    pokemon.appendChild(newImg)
+    pokemon.appendChild(label)
+    container.append(pokemon)
 }
